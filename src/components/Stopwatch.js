d1764e59cc761afa61b94a06339a33ba8c60ed36
Yashwanth <yashwanthrangu99@gmail.com>
2025-09-28 00:04:58 +0530

    Add multiple files

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -6,125 +6,161 @@ export const Stopwatch = () => {
   const [time, setTime] = useState(0);
   const [isRunning, setIsRunning] = useState(false);
   const [activeTab, setActiveTab] = useState("output");
+  const [activeFile, setActiveFile] = useState("App.js");
 
-  const codeString = `
-    import React, { useState, useEffect } from "react";
+  const files = {
+    "App.js": `
+      import React from "react";
+      import { Stopwatch } from "./components/Stopwatch";
+      
+      import "./App.css";
+      
+      function App() {
+        return (
+          <div className="App">
+            <Stopwatch />
+          </div>
+        );
+      }
+      
+      export default App;
+    `,
+    "Stopwatch.js": `
+      import React, { useState, useEffect } from "react";
 
-    import "../styles/stopwatch.css";
+      import "../styles/stopwatch.css";
 
-    export const Stopwatch = () => {
+      export const Stopwatch = () => {
         const [time, setTime] = useState(0);
         const [isRunning, setIsRunning] = useState(false);
         const [activeTab, setActiveTab] = useState("output");
 
         useEffect(() => {
-            if (isRunning) {
+          if (isRunning) {
             let timer = setInterval(() => {
                 setTime((prev) => prev + 1);
             }, 1000);
 
             return () => clearInterval(timer);
-            }
+          }
         }, [isRunning]);
 
         const formatTime = (totalSeconds) => {
-            const min = Math.floor(totalSeconds / 60);
-            const sec = totalSeconds % 60;
+          const min = Math.floor(totalSeconds / 60);
+          const sec = totalSeconds % 60;
 
-            const padMin = String(min).padStart(2, "0");
-            const padSec = String(sec).padStart(2, "0");
+          const padMin = String(min).padStart(2, "0");
+          const padSec = String(sec).padStart(2, "0");
 
-            return \`\${padMin} : \${padSec}\`;
+          return \`\${padMin} : \${padSec}\`;
         };
 
         const handlerOutput = () => {
-            setActiveTab("output");
+          setActiveTab("output");
         };
 
         const handlerCode = () => {
-            setActiveTab("code");
+          setActiveTab("code");
         };
 
         const handlerStart = () => {
-            setIsRunning(true);
+          setIsRunning(true);
         };
 
         const handlerStop = () => {
-            setIsRunning(false);
+          setIsRunning(false);
         };
 
         const handlerReset = () => {
-            setTime(0);
-            setIsRunning(false);
+          setTime(0);
+          setIsRunning(false);
         };
 
         const output = (
-            <>
-              <h3 className="timer">{formatTime(time)}</h3>
+          <>
+            <h3 className="timer">{formatTime(time)}</h3>
 
-              <div className="button-container">
-                <button
-                  type="button"
-                  className="btn btn-success"
-                  onClick={handlerStart}
-                >
-                  Start
-                </button>
-                <button 
-                  type="button" 
-                  className="btn btn-danger" 
-                  onClick={handlerStop}
-                >
-                  Stop
-                </button>
-                <button
-                  type="button"
-                  className="btn btn-secondary"
-                  onClick={handlerReset}
-                >
-                  Reset
-                </button>
-              </div>
-            </>
+            <div className="button-container">
+              <button
+                type="button"
+                className="btn btn-success"
+                onClick={handlerStart}
+              >
+                Start
+              </button>
+              <button 
+                type="button" 
+                className="btn btn-danger" 
+                onClick={handlerStop}
+              >
+                Stop
+              </button>
+              <button
+                type="button"
+                className="btn btn-secondary"
+                onClick={handlerReset}
+              >
+                Reset
+              </button>
+            </div>
+          </>
         );
 
         const code = (
-          <pre className="pre-code-container">
-            <code>{codeString}</code>
-          </pre>
+          <>
+            <div className="d-flex flex-row active-file-container">
+              {Object.keys(files).map((file) => (
+                <div
+                  key={file}
+                  className="active-file"
+                  onClick={() => setActiveFile(file)}
+                  style={{
+                    background: activeFile === file ? "#e0e0e0" : "transparent",
+                    color: activeFile === file ? "#000" : "#fff",
+                  }}
+                >
+                  {file}
+                </div>
+              ))}
+            </div>
+
+            <pre className="pre-code-container">
+              <code>{files[activeFile]}</code>
+            </pre>
+          </>
         );
            
         return (
-            <div className="stopwatch-container">
-                <h1 className="heading">STOPWATCH</h1>
+          <div className="stopwatch-container">
+            <h1 className="heading">STOPWATCH</h1>
 
-                <div className="tabs-container d-flex flex-row justify-content-evenly">
-                    <button
-                      className="tab-btn"
-                      style={{
-                        background: activeTab === "output" ? "#3cc751ff" : "#000000",
-                      }}
-                      onClick={handlerOutput}
-                    >
-                      Output
-                    </button>
-                    <button
-                      className="tab-btn"
-                      style={{
-                        background: activeTab !== "output" ? "#3cc751ff" : "#000000",
-                      }}
-                      onClick={handlerCode}
-                    >
-                      Code
-                    </button>
-                </div>
-
-                {activeTab === "output" ? output : code}
+            <div className="tabs-container d-flex flex-row justify-content-evenly">
+              <button
+                className="tab-btn"
+                style={{
+                  background: activeTab === "output" ? "#3cc751ff" : "#000000",
+                }}
+                onClick={handlerOutput}
+              >
+                Output
+              </button>
+              <button
+                className="tab-btn"
+                style={{
+                  background: activeTab !== "output" ? "#3cc751ff" : "#000000",
+                }}
+                onClick={handlerCode}
+              >
+                Code
+              </button>
             </div>
+
+            {activeTab === "output" ? output : code}
+          </div>
         );
     };
-    
-`;
+    `,
+  };
 
   useEffect(() => {
     if (isRunning) {
@@ -194,9 +230,27 @@ export const Stopwatch = () => {
   );
 
   const code = (
-    <pre className="pre-code-container">
-      <code>{codeString}</code>
-    </pre>
+    <>
+      <div className="d-flex flex-row active-file-container">
+        {Object.keys(files).map((file) => (
+          <div
+            key={file}
+            className="active-file"
+            onClick={() => setActiveFile(file)}
+            style={{
+              background: activeFile === file ? "#e0e0e0" : "transparent",
+              color: activeFile === file ? "#000" : "#fff",
+            }}
+          >
+            {file}
+          </div>
+        ))}
+      </div>
+
+      <pre className="pre-code-container">
+        <code>{files[activeFile]}</code>
+      </pre>
+    </>
   );
 
   return (
